dde50f12f641e7da1b0e4ab1460549e3b51dceb0
Jason Sturges <jason@jasonsturges.com>
2022-06-20 00:20:05 -0500

    Static directory configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/html"
-}
+  staticDirs: ["../public"],
+  framework: "@storybook/html",
+};
