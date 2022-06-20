3bc1b6cae1623198b199edad8b1ae79c916cc49b
Jason Sturges <jason@jasonsturges.com>
2022-06-20 00:20:59 -0500

    Cube story

---
+++ stories/shapes/Cube.stories.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+import { camera, canvas, renderer, scene } from "../Scene";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export default {
+  title: "Example/Shapes",
+  argTypes: {},
+};
+
+export const Cube = ({}) => {
+  camera.position.z = 2.75;
+
+  const directionalLight = new THREE.DirectionalLight(0x9099aa);
+  directionalLight.position.set(-10, 10, -10).normalize();
+  scene.add(directionalLight);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+  hemisphereLight.position.set(1, 1, 1);
+  scene.add(hemisphereLight);
+
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x00ff00,
+    metalness: 0.15,
+  });
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  return canvas;
+};
+Cube.args = {};
